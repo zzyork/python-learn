2d2eecc15e06307bd60140b4f0456c71bc17c42c
zzyork <45233837+zzyork@users.noreply.github.com>
2019-02-27 14:05:11 +0800

    Update sort test.py

--- sort test.py
+++ sort test.py
@@ -1,18 +1,18 @@
 place = ['zhangjiajie','wujiang','nanchang','fuzhou','zhangzhou','foshan']
-print(place)
+print(place) #创建列表并说出所有元素
 
 place.sort()
 print('\n')
-print(place)
+print(place) #按字母排序
 
 place.sort(reverse=True)
 print('\n')
-print(place)
+print(place) #按字母反向排序
 
 
 
 print('\n')
-print(len(place))
+print(len(place)) #输出列表长度
 
 
-print(place[-4])
+print(place[-4]) #输出列表中第-4个元素，-1为最后一个，一次类推；
