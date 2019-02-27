b7416dd0df037da699d42d3f3ded53c0dacb967e
zzyork <45233837+zzyork@users.noreply.github.com>
2019-02-27 14:22:30 +0800

    for循环学习代码

---
+++ for test.py
@@ -0,0 +1,5 @@
+magicians = ['alice','david','carolina']
+for magician in magicians:    #将列表中的元素循环进magician中;
+	print(magician.title() + ", That is a great trick!") #打印参数及字段。
+	print("I can't wait your next trick, " + magician.title() + ".\n")
+print("Thanks! everyone. that was a great magic show! ")
