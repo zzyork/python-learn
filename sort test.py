0a898ee3c77d8125137f624dfc695f4da7c529dd
zzyork <45233837+zzyork@users.noreply.github.com>
2019-02-27 14:01:32 +0800

    Create sort test.py

---
+++ sort test.py
@@ -0,0 +1,18 @@
+place = ['zhangjiajie','wujiang','nanchang','fuzhou','zhangzhou','foshan']
+print(place)
+
+place.sort()
+print('\n')
+print(place)
+
+place.sort(reverse=True)
+print('\n')
+print(place)
+
+
+
+print('\n')
+print(len(place))
+
+
+print(place[-4])
