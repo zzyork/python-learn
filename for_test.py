86fd7eb93536cad9225659da50d66bc7251a28a3
zzyork <45233837+zzyork@users.noreply.github.com>
2019-03-13 15:31:15 +0800

    Update for_test.py

--- for_test.py
+++ for_test.py
@@ -1,7 +1,13 @@
 sum = 0
 for x in [1,2,3,4,5,6,7,8,9,10]:
-	sum = sum + x
+	sum = sum + x					将数组与sum累加
 print(sum)
 
 
 print(list(range(101)))
+
+
+sum = 0
+for x in range(101):  #从0-100循环
+	sum = sum + x
+print (sum)
